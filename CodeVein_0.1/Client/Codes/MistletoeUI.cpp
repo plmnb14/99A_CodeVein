ef6c646f8d3c7905b316aa2654c19a294538a8d7
anjiwon <rala2580@naver.com>
2020-04-18 14:57:42 +0900

    1) 겨우살이UI Texture 추가 2) 겨우살이UI 클래스 추가

---
+++ CodeVein_0.1/Client/Codes/MistletoeUI.cpp
@@ -0,0 +1,140 @@
+#include "stdafx.h"
+#include "..\Headers\MistletoeUI.h"
+
+CMistletoeUI::CMistletoeUI(_Device pDevice)
+	: CUI(pDevice)
+{
+}
+
+CMistletoeUI::CMistletoeUI(const CMistletoeUI & rhs)
+	: CUI(rhs)
+{
+}
+
+HRESULT CMistletoeUI::Ready_GameObject_Prototype()
+{
+	CUI::Ready_GameObject_Prototype();
+	return NOERROR;
+}
+
+HRESULT CMistletoeUI::Ready_GameObject(void * pArg)
+{
+	if (FAILED(Add_Component()))
+		return E_FAIL;
+	CUI::Ready_GameObject(pArg);
+	m_bIsActive = false;
+	return NOERROR;
+}
+
+_int CMistletoeUI::Update_GameObject(_double TimeDelta)
+{
+	CUI::Update_GameObject(TimeDelta);
+	m_pRendererCom->Add_RenderList(RENDER_UI, this);
+	Compute_ViewZ(&m_pTransformCom->Get_Pos());
+	return NO_EVENT;
+}
+
+_int CMistletoeUI::Late_Update_GameObject(_double TimeDelta)
+{
+	if (nullptr == m_pRendererCom)
+		return E_FAIL;
+
+	m_matView = g_pManagement->Get_Transform(D3DTS_VIEW);
+	m_matProj = g_pManagement->Get_Transform(D3DTS_PROJECTION);
+
+	return NO_EVENT;
+}
+
+HRESULT CMistletoeUI::Render_GameObject()
+{
+	if (nullptr == m_pShaderCom ||
+		nullptr == m_pBufferCom)
+		return E_FAIL;
+
+	if (FAILED(SetUp_ConstantTable()))
+		return E_FAIL;
+
+	m_pShaderCom->Begin_Shader();
+	m_pShaderCom->Begin_Pass(5);
+
+	m_pBufferCom->Render_VIBuffer();
+	m_pShaderCom->End_Pass();
+	m_pShaderCom->End_Shader();
+
+	return NOERROR;
+}
+
+HRESULT CMistletoeUI::Add_Component()
+{
+	// For.Com_Transform
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Transform", L"Com_Transform", (CComponent**)&m_pTransformCom)))
+		return E_FAIL;
+
+	// For.Com_Renderer
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Renderer", L"Com_Renderer", (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	// For.Com_Texture
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Tex_MistletoeUI", L"Com_Texture", (CComponent**)&m_pTextureCom)))
+		return E_FAIL;
+
+	// For.Com_Shader
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Shader_UI", L"Com_Shader", (CComponent**)&m_pShaderCom)))
+		return E_FAIL;
+
+	// for.Com_VIBuffer
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"VIBuffer_Rect", L"Com_VIBuffer", (CComponent**)&m_pBufferCom)))
+		return E_FAIL;
+
+	return NOERROR;
+}
+
+HRESULT CMistletoeUI::SetUp_ConstantTable()
+{
+	if (nullptr == m_pShaderCom)
+		return E_FAIL;
+
+	if (FAILED(m_pShaderCom->Set_Value("g_matWorld", &m_pTransformCom->Get_WorldMat(), sizeof(_mat))))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Value("g_matView", &m_matView, sizeof(_mat))))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Value("g_matProj", &m_matProj, sizeof(_mat))))
+		return E_FAIL;
+
+	if (FAILED(m_pTextureCom->SetUp_OnShader("g_DiffuseTexture", m_pShaderCom, m_iIndex)))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Value("g_fAlpha", &m_fAlpha, sizeof(_float))))
+		return E_FAIL;
+	return NOERROR;
+}
+
+CMistletoeUI * CMistletoeUI::Create(_Device pGraphic_Device)
+{
+	CMistletoeUI* pInstance = new CMistletoeUI(pGraphic_Device);
+
+	if (FAILED(pInstance->Ready_GameObject_Prototype()))
+		Safe_Release(pInstance);
+
+	return pInstance;
+}
+
+CGameObject * CMistletoeUI::Clone_GameObject(void * pArg)
+{
+	CMistletoeUI* pInstance = new CMistletoeUI(*this);
+
+	if (FAILED(pInstance->Ready_GameObject(pArg)))
+		Safe_Release(pInstance);
+
+	return pInstance;
+}
+
+void CMistletoeUI::Free()
+{
+	Safe_Release(m_pTransformCom);
+	Safe_Release(m_pBufferCom);
+	Safe_Release(m_pShaderCom);
+	Safe_Release(m_pTextureCom);
+	Safe_Release(m_pRendererCom);
+
+	CUI::Free();
+}
