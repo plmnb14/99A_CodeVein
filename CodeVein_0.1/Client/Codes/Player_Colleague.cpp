3f5f9a9aaf1b28290964f53927782cd2c0ea53f0
Ymm1 <planner07@naver.com>
2020-04-19 23:34:14 +0900

    1. Player_Colleague 핫픽스

--- CodeVein_0.1/Client/Codes/Player_Colleague.cpp
+++ CodeVein_0.1/Client/Codes/Player_Colleague.cpp
@@ -430,7 +430,7 @@ void CPlayer_Colleague::Set_AniEvent()
 			{
 				// 전투 대기 상태 - 간보는 상태?
 				CollAtt_Waiting();
-				m_pDynamicMesh->SetUp_Animation()
+				//m_pDynamicMesh->SetUp_Animation();
 				break;
 			}
 			case CPlayer_Colleague::Att_Normal:
