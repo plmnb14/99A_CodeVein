b652ec69a6f63c72704b6c31e39026ac07442db5
GyuMyeongShim <sgm8626@gmail.com>
2020-04-21 11:12:54 +0900

    1.몬스터 죽음, 밀림, 생성시 기본인자

--- CodeVein_0.1/Client/Codes/Weapon.cpp
+++ CodeVein_0.1/Client/Codes/Weapon.cpp
@@ -184,6 +184,9 @@ void CWeapon::OnCollisionEvent(list<CGameObject*> plistGameObject)
 	//게임 오브젝트를 받아와서
 	for (auto& iter : plistGameObject)
 	{
+		if(false == iter->Get_Enable())
+			continue;
+
 		// 1 : 다 의 기술일 경우, 기록을 켜야 한대만 맞는다.
 		if (m_bRecordCollision)
 		{
@@ -231,11 +234,11 @@ void CWeapon::OnCollisionEvent(list<CGameObject*> plistGameObject)
 							_uint max = (_uint)(m_tObjParam.fDamage + (m_tObjParam.fDamage * 0.2f));
 
 							//피격시 밀림처리.....
-							//memcpy(vHitDir, &(m_pmatParent->_41), sizeof(_v3));
+							memcpy(vHitDir, &(m_pmatParent->_41), sizeof(_v3));
 
-							//V3_NORMAL(&m_tObjParam.vHitDir, &(ExceptY - vHitDir));
+							V3_NORMAL(&m_tObjParam.vHitDir, &(TARGET_TO_TRANS(iter)->Get_Pos() - vHitDir));
 
-							//iter->Set_Target_HitDir(m_tObjParam.vHitDir);
+							iter->Set_Target_HitDir(m_tObjParam.vHitDir);
 
 							iter->Add_Target_Hp(-(_float)CALC::Random_Num(min , max) * m_fSkillPercent);
 							g_pManagement->Create_Hit_Effect(vecIter, vecCol, TARGET_TO_TRANS(iter));
