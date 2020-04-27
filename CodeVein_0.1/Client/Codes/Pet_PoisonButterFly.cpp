fa1198f8f2ba7311257389f2289e858133cd5ad7
GyuMyeongShim <sgm8626@gmail.com>
2020-04-27 21:50:01 +0900

    Revert "Revert "1.독나비펫 틀 생성""
    This reverts commit dcd2d4bde3c061f02900f2778c28e43328cf55e8.

---
+++ CodeVein_0.1/Client/Codes/Pet_PoisonButterFly.cpp
@@ -0,0 +1,546 @@
+#include "stdafx.h"
+#include "..\Headers\Pet_PoisonButterFly.h"
+
+CPet_PoisonButterFly::CPet_PoisonButterFly(LPDIRECT3DDEVICE9 pGraphic_Device)
+	:CPet(pGraphic_Device)
+{
+}
+
+CPet_PoisonButterFly::CPet_PoisonButterFly(const CPet_PoisonButterFly & rhs)
+	:CPet(rhs)
+{
+}
+
+HRESULT CPet_PoisonButterFly::Ready_GameObject_Prototype()
+{
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::Ready_GameObject(void * pArg)
+{
+	if (FAILED(Add_Component(pArg)))
+		return E_FAIL;
+
+	m_pTransformCom->Set_Pos(_v3(1.f, 0.f, 1.f));
+	m_pTransformCom->Set_Scale(_v3(0.5f,0.5f,0.5f));
+
+	Ready_Status(pArg);
+	Ready_BoneMatrix(pArg);
+	Ready_Collider(pArg);
+	Ready_Weapon(pArg);
+
+	m_pMonsterUI = static_cast<CMonsterUI*>(g_pManagement->Clone_GameObject_Return(L"GameObject_MonsterHPUI", pArg));
+	m_pMonsterUI->Set_Target(this);
+	m_pMonsterUI->Set_Bonmatrix(m_matBone[Bone_Head]);
+	m_pMonsterUI->Ready_GameObject(NULL);
+
+	m_pTarget = g_pManagement->Get_GameObjectBack(L"Layer_Player", SCENE_MORTAL);
+
+	if (nullptr != m_pTarget)
+	{
+		Safe_AddRef(m_pTarget);
+
+		m_pPlayerTransform = TARGET_TO_TRANS(g_pManagement->Get_GameObjectBack(L"Layer_Player", SCENE_MORTAL));
+		Safe_AddRef(m_pTargetTransform);
+	}
+
+	return S_OK;
+}
+
+_int CPet_PoisonButterFly::Update_GameObject(_double TimeDelta)
+{
+	if (false == m_bEnable)
+		return NO_EVENT;
+
+	CGameObject::Update_GameObject(TimeDelta);
+
+	m_pMonsterUI->Update_GameObject(TimeDelta);
+
+	Check_Target();
+	Check_PosY();
+	Check_Hit();
+	Check_Dist();
+	Check_AniEvent();
+	Function_CoolDown();
+
+	m_pMeshCom->SetUp_Animation(m_eState);
+
+	MONSTER_STATETYPE::DEAD != m_eFirstCategory ? Check_CollisionEvent() : Check_DeadEffect(TimeDelta);
+
+	return NO_EVENT;
+}
+
+_int CPet_PoisonButterFly::Late_Update_GameObject(_double TimeDelta)
+{
+	if (false == m_bEnable)
+		return NO_EVENT;
+
+	IF_NULL_VALUE_RETURN(m_pRendererCom, E_FAIL);
+
+	if (FAILED(m_pRendererCom->Add_RenderList(RENDER_NONALPHA, this)))
+		return E_FAIL;
+	if (FAILED(m_pRendererCom->Add_RenderList(RENDER_MOTIONBLURTARGET, this)))
+		return E_FAIL;
+
+	m_dTimeDelta = TimeDelta;
+
+	IF_NOT_NULL(m_pWeapon)
+		m_pWeapon->Late_Update_GameObject(TimeDelta);
+
+	return NO_EVENT;
+}
+
+HRESULT CPet_PoisonButterFly::Render_GameObject()
+{
+	IF_NULL_VALUE_RETURN(m_pShaderCom, E_FAIL);
+	IF_NULL_VALUE_RETURN(m_pMeshCom, E_FAIL);
+
+	m_pMeshCom->Play_Animation(DELTA_60 * m_dAniPlayMul);
+
+	if (FAILED(SetUp_ConstantTable()))
+		return E_FAIL;
+
+	m_pShaderCom->Begin_Shader();
+
+	_uint iNumMeshContainer = _uint(m_pMeshCom->Get_NumMeshContainer());
+
+	for (_uint i = 0; i < _uint(iNumMeshContainer); ++i)
+	{
+		_uint iNumSubSet = (_uint)m_pMeshCom->Get_NumMaterials(i);
+
+		m_pMeshCom->Update_SkinnedMesh(i);
+
+		for (_uint j = 0; j < iNumSubSet; ++j)
+		{
+			if (MONSTER_STATETYPE::DEAD != m_eFirstCategory)
+				m_iPass = m_pMeshCom->Get_MaterialPass(i, j);
+
+			m_pShaderCom->Begin_Pass(m_iPass);
+
+			m_pShaderCom->Set_DynamicTexture_Auto(m_pMeshCom, i, j);
+
+			m_pShaderCom->Commit_Changes();
+
+			m_pMeshCom->Render_Mesh(i, j);
+
+			m_pShaderCom->End_Pass();
+		}
+	}
+
+	m_pShaderCom->End_Shader();
+
+	IF_NOT_NULL(m_pWeapon)
+		m_pWeapon->Update_GameObject(m_dTimeDelta);
+
+	Update_Collider();
+	Render_Collider();
+
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::Render_GameObject_SetPass(CShader * pShader, _int iPass)
+{
+	IF_NULL_VALUE_RETURN(pShader, E_FAIL);
+	IF_NULL_VALUE_RETURN(m_pMeshCom, E_FAIL);
+
+	if (FAILED(SetUp_ConstantTable()))
+		return E_FAIL;
+
+	if (FAILED(pShader->Set_Value("g_matWorld", &m_pTransformCom->Get_WorldMat(), sizeof(_mat))))
+		return E_FAIL;
+
+	_mat ViewMatrix = g_pManagement->Get_Transform(D3DTS_VIEW);
+	_mat ProjMatrix = g_pManagement->Get_Transform(D3DTS_PROJECTION);
+	if (FAILED(pShader->Set_Value("g_matLastWVP", &m_matLastWVP, sizeof(_mat))))
+		return E_FAIL;
+
+	m_matLastWVP = m_pTransformCom->Get_WorldMat() * ViewMatrix * ProjMatrix;
+
+	_uint iNumMeshContainer = _uint(m_pMeshCom->Get_NumMeshContainer());
+
+	for (_uint i = 0; i < _uint(iNumMeshContainer); ++i)
+	{
+		_uint iNumSubSet = (_uint)m_pMeshCom->Get_NumMaterials(i);
+
+		m_pMeshCom->Update_SkinnedMesh(i);
+
+		for (_uint j = 0; j < iNumSubSet; ++j)
+		{
+			pShader->Begin_Pass(iPass);
+
+			m_pMeshCom->Render_Mesh(i, j);
+
+			pShader->End_Pass();
+		}
+	}
+
+	return S_OK;
+}
+
+void CPet_PoisonButterFly::Update_Collider()
+{
+	_ulong matrixIdx = Bone_Body;
+
+	for (auto& vector_iter : m_vecAttackCol)
+	{
+		_mat matTemp = *m_matBone[matrixIdx] * m_pTransformCom->Get_WorldMat();
+
+		_v3 ColPos = _v3(matTemp._41, matTemp._42, matTemp._43);
+
+		vector_iter->Update(ColPos);
+
+		++matrixIdx;
+	}
+
+	matrixIdx = 0;
+
+	for (auto& iter : m_vecPhysicCol)
+	{
+		_mat tmpMat = *m_matBone[matrixIdx] * m_pTransformCom->Get_WorldMat();
+
+		_v3 ColPos = _v3(tmpMat._41, tmpMat._42, tmpMat._43);
+
+		iter->Update(ColPos);
+
+		++matrixIdx;
+	}
+
+	m_pCollider->Update(m_pTransformCom->Get_Pos() + _v3(0.f, m_pCollider->Get_Radius().y, 0.f));
+
+	return;
+}
+
+void CPet_PoisonButterFly::Render_Collider()
+{
+	for (auto& iter : m_vecAttackCol)
+		g_pManagement->Gizmo_Draw_Sphere(iter->Get_CenterPos(), iter->Get_Radius().x);
+
+	for (auto& iter : m_vecPhysicCol)
+		g_pManagement->Gizmo_Draw_Sphere(iter->Get_CenterPos(), iter->Get_Radius().x);
+
+	return;
+}
+
+void CPet_PoisonButterFly::Check_CollisionEvent()
+{
+	Check_CollisionPush();
+	Check_CollisionHit(g_pManagement->Get_GameObjectList(L"Layer_Boss", SCENE_MORTAL));
+	Check_CollisionHit(g_pManagement->Get_GameObjectList(L"Layer_Monster", SCENE_MORTAL));
+
+	return;
+}
+
+void CPet_PoisonButterFly::Check_CollisionPush()
+{
+	list<CGameObject*> tmpList[3];
+
+	tmpList[0] = g_pManagement->Get_GameObjectList(L"Layer_Player", SCENE_MORTAL);
+	tmpList[1] = g_pManagement->Get_GameObjectList(L"Layer_Monster", SCENE_STAGE);
+	tmpList[2] = g_pManagement->Get_GameObjectList(L"Layer_Boss", SCENE_STAGE);
+
+	for (auto& list_iter : tmpList)
+	{
+		for (auto& Obj_iter : list_iter)
+		{
+			CCollider* pCollider = TARGET_TO_COL(Obj_iter);
+
+			if (m_pCollider->Check_Sphere(pCollider, m_pTransformCom->Get_Axis(AXIS_Z), m_fSkillMoveSpeed_Cur * DELTA_60))
+			{
+				CTransform* pTrans = TARGET_TO_TRANS(Obj_iter);
+				CNavMesh*   pNav = TARGET_TO_NAV(Obj_iter);
+
+				_v3 vDir = m_pTransformCom->Get_Pos() - pTrans->Get_Pos();
+				V3_NORMAL_SELF(&vDir);
+
+				vDir.y = 0;
+
+				pTrans->Set_Pos(pNav->Move_OnNaviMesh(NULL, &pTrans->Get_Pos(), &vDir, m_pCollider->Get_Length().x));
+			}
+		}
+	}
+
+	return;
+}
+
+void CPet_PoisonButterFly::Check_CollisionHit(list<CGameObject*> plistGameObject)
+{
+	if (false == m_tObjParam.bIsAttack)
+		return;
+
+	_bool bFirst = true;
+
+	for (auto& iter : plistGameObject)
+	{
+		if (false == iter->Get_Target_CanHit())
+			continue;
+
+		for (auto& vecIter : m_vecAttackCol)
+		{
+			if (false == vecIter->Get_Enabled())
+				continue;
+
+			bFirst = true;
+
+			for (auto& vecCol : iter->Get_PhysicColVector())
+			{
+				if (vecIter->Check_Sphere(vecCol))
+				{
+					if (bFirst)
+					{
+						bFirst = false;
+						continue;
+					}
+
+					if (false == iter->Get_Target_IsDodge())
+					{
+						iter->Set_Target_CanHit(false);
+						iter->Add_Target_Hp(m_tObjParam.fDamage);
+
+						if (iter->Get_Target_IsHit())
+						{
+							iter->Set_HitAgain(true);
+						}
+					}
+
+					vecIter->Set_Enabled(false);
+
+					g_pManagement->Create_Hit_Effect(vecIter, vecCol, TARGET_TO_TRANS(iter));
+
+					break;
+				}
+				else
+				{
+					if (bFirst)
+						break;
+				}
+			}
+		}
+	}
+
+	return;
+}
+
+void CPet_PoisonButterFly::Check_Target()
+{
+	return;
+}
+
+HRESULT CPet_PoisonButterFly::Add_Component(void * pArg)
+{
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Transform", L"Com_Transform", (CComponent**)&m_pTransformCom)))
+		return E_FAIL;
+
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Renderer", L"Com_Renderer", (CComponent**)&m_pRendererCom)))
+		return E_FAIL;
+
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Shader_Mesh", L"Com_Shader", (CComponent**)&m_pShaderCom)))
+		return E_FAIL;
+
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Mesh_PoisonButterFly", L"Com_Mesh", (CComponent**)&m_pMeshCom)))
+		return E_FAIL;
+
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"NavMesh", L"Com_NavMesh", (CComponent**)&m_pNavMesh)))
+		return E_FAIL;
+
+	if (FAILED(CGameObject::Add_Component(SCENE_STATIC, L"Collider", L"Com_Collider", (CComponent**)&m_pCollider)))
+		return E_FAIL;
+
+	m_pCollider->Set_Radius(_v3{ 0.5f, 0.5f, 0.5f });
+	m_pCollider->Set_Dynamic(true);
+	m_pCollider->Set_Type(COL_SPHERE);
+	m_pCollider->Set_CenterPos(m_pTransformCom->Get_Pos() + _v3{ 0.f , m_pCollider->Get_Radius().y , 0.f });
+
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::SetUp_ConstantTable()
+{
+	IF_NULL_VALUE_RETURN(m_pShaderCom, E_FAIL);
+
+	CManagement* pManagement = CManagement::Get_Instance();
+	IF_NULL_VALUE_RETURN(pManagement, E_FAIL);
+
+	Safe_AddRef(pManagement);
+
+	if (FAILED(m_pShaderCom->Set_Value("g_matWorld", &m_pTransformCom->Get_WorldMat(), sizeof(_mat))))
+		return E_FAIL;
+
+	_mat ViewMatrix = pManagement->Get_Transform(D3DTS_VIEW);
+	_mat ProjMatrix = pManagement->Get_Transform(D3DTS_PROJECTION);
+
+	if (FAILED(m_pShaderCom->Set_Value("g_matView", &ViewMatrix, sizeof(_mat))))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Value("g_matProj", &ProjMatrix, sizeof(_mat))))
+		return E_FAIL;
+	if (FAILED(g_pDissolveTexture->SetUp_OnShader("g_FXTexture", m_pShaderCom)))
+		return E_FAIL;
+	if (FAILED(m_pShaderCom->Set_Value("g_fFxAlpha", &m_fFXAlpha, sizeof(_float))))
+		return E_FAIL;
+
+	Safe_Release(pManagement);
+
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::Ready_Status(void * pArg)
+{
+	m_tObjParam.fDamage = -500.f;
+	m_tObjParam.fHp_Max = 2000.f;
+	m_tObjParam.fArmor_Max = 10.f;
+
+	m_fRecognitionRange = 15.f;
+	m_fShotRange = 10.f;
+	m_fAtkRange = 5.f;
+	m_fPersonalRange = 2.f;
+	m_iDodgeCountMax = 5;
+
+	m_eFirstCategory = MONSTER_STATETYPE::IDLE;
+	m_tObjParam.fHp_Cur = m_tObjParam.fHp_Max;
+	m_tObjParam.fArmor_Cur = m_tObjParam.fArmor_Max;
+
+	m_tObjParam.bCanHit = true; //맞기 가능
+	m_tObjParam.bIsHit = false;	//맞기 진행중 아님
+	m_tObjParam.bCanAttack = true; //공격 가능
+	m_tObjParam.bIsAttack = false; //공격 진행중 아님
+	m_tObjParam.bCanDodge = true; //회피 가능
+	m_tObjParam.bIsDodge = false;  //회피 진행중 아님
+
+	m_bCanPlayDead = false; //죽음 애니 진행시 true;
+	m_bInRecognitionRange = false; //인지 범위 여부
+	m_bInAtkRange = false; //공격 범위 여부
+	m_bCanChase = false; //추격 여부
+	m_bCanCoolDown = false; //쿨타임 여부
+	m_bIsCoolDown = false; //쿨타임 진행중 여부
+
+	m_bCanChooseAtkType = true; //공격타입 고정용
+	m_bIsCombo = false; //콤보 진행중
+
+	m_bCanMoveAround = true; //경계 여부
+	m_bIsMoveAround = false; //경게 진행중
+
+	m_bCanIdle = true; //일상 가능
+	m_bIsIdle = false; //일상 진행중 아님
+
+	m_dTimeDelta = 0;
+	m_dAniPlayMul = 1;
+
+	m_fSkillMoveSpeed_Cur = 0.f;
+	m_fSkillMoveSpeed_Max = 0.f;
+	m_fSkillMoveAccel_Cur = 0.5f;
+	m_fSkillMoveAccel_Max = 0.f;
+	m_fSkillMoveMultiply = 1.f;
+
+	m_fCoolDownMax = 0.f;
+	m_fCoolDownCur = 0.f;
+
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::Ready_Weapon(void * pArg)
+{
+	m_pWeapon = static_cast<CWeapon*>(g_pManagement->Clone_GameObject_Return(L"GameObject_Weapon", NULL));
+
+	switch (m_eWeaponState)
+	{
+	case WEAPON_STATE::WEAPON_Gun:
+		m_pWeapon->Change_WeaponData(CWeapon::Wpn_Gun_Black);
+		break;
+	case WEAPON_STATE::WEAPON_Halberd:
+		m_pWeapon->Change_WeaponData(CWeapon::Wpn_Halberd_Black);
+		break;
+	case WEAPON_STATE::WEAPON_Hammer:
+		m_pWeapon->Change_WeaponData(CWeapon::Wpn_Hammer_Black);
+		break;
+	case WEAPON_STATE::WEAPON_LSword:
+		m_pWeapon->Change_WeaponData(CWeapon::Wpn_LSword_Black);
+		break;
+	case WEAPON_STATE::WEAPON_SSword:
+		m_pWeapon->Change_WeaponData(CWeapon::Wpn_SSword_Black);
+		break;
+	}
+
+	D3DXFRAME_DERIVED*	pFrame = (D3DXFRAME_DERIVED*)m_pMeshCom->Get_BonInfo("RightHandAttach");
+	m_pWeapon->Set_AttachBoneMartix(&pFrame->CombinedTransformationMatrix);
+	m_pWeapon->Set_ParentMatrix(&m_pTransformCom->Get_WorldMat());
+
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::Ready_Collider(void * pArg)
+{
+	m_vecPhysicCol.reserve(2);
+	m_vecAttackCol.reserve(2);
+
+	_float fRadius;
+	CCollider* pCollider = nullptr;
+
+	// 첫번째 콜라이더는 경계 체크용 콜라이더
+	IF_NULL_VALUE_RETURN(pCollider = static_cast<CCollider*>(g_pManagement->Clone_Component(SCENE_STATIC, L"Collider")), E_FAIL);
+	fRadius = 1.2f;
+
+	pCollider->Set_Radius(_v3{ fRadius, fRadius, fRadius });
+	pCollider->Set_Dynamic(true);
+	pCollider->Set_Type(COL_SPHERE);
+	pCollider->Set_CenterPos(_v3(m_matBone[Bone_Range]->_41, m_matBone[Bone_Range]->_42, m_matBone[Bone_Range]->_43));
+	pCollider->Set_Enabled(true);
+	m_vecPhysicCol.push_back(pCollider);
+
+	IF_NULL_VALUE_RETURN(pCollider = static_cast<CCollider*>(g_pManagement->Clone_Component(SCENE_STATIC, L"Collider")), E_FAIL);
+	fRadius = 0.7f;
+
+	pCollider->Set_Radius(_v3{ fRadius, fRadius, fRadius });
+	pCollider->Set_Dynamic(true);
+	pCollider->Set_Type(COL_SPHERE);
+	pCollider->Set_CenterPos(_v3(m_matBone[Bone_Body]->_41, m_matBone[Bone_Body]->_42, m_matBone[Bone_Body]->_43));
+	pCollider->Set_Enabled(true);
+	m_vecPhysicCol.push_back(pCollider);
+
+
+	return S_OK;
+}
+
+HRESULT CPet_PoisonButterFly::Ready_BoneMatrix(void * pArg)
+{
+	D3DXFRAME_DERIVED*	pFrame = nullptr;
+	IF_NULL_VALUE_RETURN(pFrame = (D3DXFRAME_DERIVED*)m_pMeshCom->Get_BonInfo("Spine", 0), E_FAIL);
+	m_matBone[Bone_Range] = &pFrame->CombinedTransformationMatrix;
+	m_matBone[Bone_Body] = &pFrame->CombinedTransformationMatrix;
+
+	IF_NULL_VALUE_RETURN(pFrame = (D3DXFRAME_DERIVED*)m_pMeshCom->Get_BonInfo("Head", 0), E_FAIL);
+	m_matBone[Bone_Head] = &pFrame->CombinedTransformationMatrix;
+
+	return S_OK;
+}
+
+CPet_PoisonButterFly* CPet_PoisonButterFly::Create(LPDIRECT3DDEVICE9 pGraphic_Device)
+{
+	CPet_PoisonButterFly* pInstance = new CPet_PoisonButterFly(pGraphic_Device);
+
+	if (FAILED(pInstance->Ready_GameObject_Prototype()))
+	{
+		MSG_BOX("Failed To Creating CPet_PoisonButterFly");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+CGameObject* CPet_PoisonButterFly::Clone_GameObject(void * pArg)
+{
+	CPet_PoisonButterFly* pInstance = new CPet_PoisonButterFly(*this);
+
+	if (FAILED(pInstance->Ready_GameObject(pArg)))
+	{
+		MSG_BOX("Failed To Cloned CPet_PoisonButterFly");
+		Safe_Release(pInstance);
+	}
+
+	return pInstance;
+}
+
+void CPet_PoisonButterFly::Free()
+{
+	CPet::Free();
+
+	return;
+}
