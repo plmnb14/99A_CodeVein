d0aff32ce99b25fee56601065d1b0a6196ca4ba0
anjiwon <ra258065@gmail.com>
2020-06-14 00:09:06 +0900

    1) HP바 줄어드는 속도 변경 2) UI 매니저에 Get_LoadingScreen()함수 추가 - 페이드인아웃

--- CodeVein_0.1/Client/Codes/PlayerHP.cpp
+++ CodeVein_0.1/Client/Codes/PlayerHP.cpp
@@ -271,7 +271,7 @@ void CPlayerHP::SetUp_State(_double TimeDelta)
 	if (m_fOldHP <= m_fPlayerHP)
 		m_fOldHP = m_fPlayerHP;
 	else
-		m_fOldHP -= TimeDelta * 20.f;
+		m_fOldHP -= _float(TimeDelta) * 30.f;
 
 	if (m_fOldHP >= m_fTotalHP)
 		m_fOldHP = m_fTotalHP;
